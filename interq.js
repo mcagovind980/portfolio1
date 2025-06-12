f54bd89efd3551ecd0ba61106f3762b3fb1bb625
mcagovind980 <kg878672@gmail.com>
2025-06-12 16:15:15 -0700

    Add files via upload

---
+++ interq.js
@@ -0,0 +1,72 @@
+// Assugnment
+
+// let a=4;
+// let b=6;
+// console.log(a+b);
+// 2Quesitons
+// let A=5;
+// let B=5;
+// if(B<A&&A<10)
+// {
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+
+
+// 3Question
+// A=120;
+// B=2;
+// if(A%10==0&&B%10==0 )
+// {
+//     console.log("true");
+// }
+
+// if(B%10==0&&A%10!=0||B%10!=0&&A%10==0)
+//     {
+//     console.log("true");
+
+// }
+
+// else 
+// {
+//     console.log("false");
+
+// }
+
+
+
+// 5Question
+// let a=[4,2,3,4,3,4,2,3];
+// let r=a[5];
+// console.log(r);
+// let numbers = [10, 20, 30, 40, 50];
+// let fourthElement = numbers[3];
+// console.log(fourthElement); // Output: 40
+
+
+// 6 Question
+// let A=27;
+// let B=2;
+// let Z=A%B;
+// console.log(Z);
+
+// 7 Question
+
+// let A=4;
+// let B=5;
+// console.log(A*B);
+
+
+// 8 Question
+
+// let A=90;
+// let B=40;
+// let C=70;
+// sum=A+B+C;
+// console.log(sum);
+// console.log((sum/300)*100);
+
+
